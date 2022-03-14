bf24f3a4b4134e11256d2a243a2723be2ad201f1
Gaurav Sarodkar <99338400+Gaurav64646464@users.noreply.github.com>
2022-03-14 17:39:35 +0530

    Update BookCollection.java

--- BookCollection.java
+++ BookCollection.java
@@ -1,23 +1,22 @@
 package com.company;
 
+
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BookCollection {
 
-    private static Logger logger = MyLoggerClass.getLOGGER();
-    private final HashMap<String, List<Book>> catalogue;
-    private final List<Book> bookList = new ArrayList<Book>();
+    private  static Logger logger = Logger.getLogger(BookCollection.class.getName());
+
+    private final List<Book> bookList = new ArrayList<>();
     private static final List<User> users = new ArrayList<>();
-    private final List<Author> author=new ArrayList<Author>();
-    public BookCollection() {
-        catalogue = new HashMap<String, List<Book>>();
-    }
+
+
 
     ///////////////////////////add book/////////////////////////////////////
-    public Book addBook(int isbn, Set<String> authorNames, String bookName, String keywords, String bookStatus, String rating, Owner owner) {
-        Book book = new Book(isbn, authorNames, bookName, keywords, bookStatus, rating, owner);
+    public Book addBook(int isbn, Set<String> authorNames, String bookName, String keywords, int rating, User users) {
+        Book book = new Book(isbn, authorNames, bookName, keywords, rating, users);
         bookList.add(book);
         return book;
     }
@@ -27,22 +26,16 @@ public class BookCollection {
     }
 
     public void viewBookList() {
-        logger.log(Level.INFO,"-------------------------------------------------------------");
         Iterator<Book> i=bookList.iterator();
         while (i.hasNext()){
             Book book=i.next();
-            logger.log(Level.INFO,""+book);
+            logger.log(Level.SEVERE,()->"Book Name :"+book);
         }
-        logger.log(Level.INFO,"-------------------------------------------------------------");
-        /*logger.log(Level.INFO, "You have " + bookList.size() + " user present in the booklist");
-        for (int i = 0; i < bookList.size(); i++) {
-            logger.log(Level.INFO, (i + 1) + ".  " + bookList.get(i));
-        }*/
     }
-    ////////////////////////add book/////////////////
+    /*********************************add book***************************************/
 
 
-    ////////////////////////////// Add User/////////////////////////
+    /********************************Add User**************************************/
     public User addUser(String email, String name) {
 
         if (getUserFromEmail(email) != null) {
@@ -79,48 +72,34 @@ public class BookCollection {
         }
         return null;
     }
-
-    public List<User> getUserList() {
-        return users;
-    }
-
-    public void viewAllUsers() {
-        logger.log(Level.INFO, "You have " + users.size() + " user present in the userlist");
-
-        for (int i = 0; i < users.size(); i++) {
-            logger.log(Level.INFO, (i + 1) + ".  " + users.get(i));
-        }
-    }
-/////////////////////////Add User////////////////////////////////
-///////////////////////   Search by bookName    ////////////////////////
-public  void searchbybookName( String bookName){
+    /*******************************Add User*********************************/
+/*****************************   Search by bookName  ************************/
+public  void searchByBookName( String bookName){
     boolean found=false;
-    logger.log(Level.INFO,"-------------------------------------------------------------");
     Iterator<Book> i=bookList.iterator();
     while (i.hasNext()){
         Book book=i.next();
         if(book.getBookName().equals(bookName))
-            logger.log(Level.INFO,""+book);
+            logger.log(Level.INFO,()->" bookname: "+book);
         found=true;
     }
     if(!found){
         logger.log(Level.INFO,"book not found");
     }
-    logger.log(Level.INFO,"-------------------------------------------------------------");
 }
 
-////////////////////////     Search by bookname      /////////////////////
+ /********************************Search by bookname*****************************/
 
-///////////////////////  Search by Authorname    ///////////////////
+/*********************************  Search by Authorname  ***********************/
 
-    public  void searchbyAuthorName(Set<String> authorName){
+    public  void searchByAuthorName(Set<String> authorName){
         boolean found=false;
         logger.log(Level.INFO,"-------------------------------------------------------------");
         Iterator<Book> i=bookList.iterator();
         while (i.hasNext()){
             Book book=i.next();
             if(book.getAuthorNames().equals(authorName))
-                logger.log(Level.INFO,"found Author:"+book);
+                logger.log(Level.INFO,()->"found Author:"+book);
             found=true;
         }
         if(!found){
@@ -128,29 +107,27 @@ public  void searchbybookName( String bookName){
         }
         logger.log(Level.INFO,"-------------------------------------------------------------");
     }
-////////////////////////////////////  Search by AuthorName  ////////////////////////////////////
+/******************************************  Search by AuthorName  *********************************/
 
 
 
 
-/////////////////////////////////  Search by Keyword   /////////////////////////////////////
+/**************************  Search by Keyword   ***********************************/
 
     public  void searchByKeyword( String keyword){
         boolean found=false;
-        logger.log(Level.INFO,"-------------------------------------------------------------");
         Iterator<Book> i=bookList.iterator();
         while (i.hasNext()){
             Book book=i.next();
             if(book.getKeywords().equals(keyword))
-                logger.log(Level.INFO,""+book);
+                logger.log(Level.SEVERE,()->"BookName:"+book);
             found=true;
         }
         if(!found){
             logger.log(Level.INFO,"book not found");
         }
-        logger.log(Level.INFO,"-------------------------------------------------------------");
     }
-    ///////////////////////////////  Search by keyword  /////////////////////////////////////////
+    /********************************  Search by keyword  **********************************/
 
 
     }
